7c47c395abf797d0f8ee0879c80438c42263a383
shailen <shailen@lumkani.com>
2019-11-14 12:10:02 +0200

    Updated docs

--- mock/src/store.js
+++ mock/src/store.js
@@ -1,32 +1,5 @@
-// import Vue from 'vue'
-
 import { Store } from 'veduxjs'
 
 export const store = new Store({
   text: null,
 })
-
-// // export const store = Vue.observable({
-// //   text: null
-// // })
-
-// class Store {
-//   constructor(state = {}) {
-//     this.root = Vue.observable({ state })
-//   }
-
-//   commit(handler) {
-//     const newState = handler({ ...this.root.state })
-    
-//     const mergedState = {
-//       ...this.root.state,
-//       ...newState
-//     }
-
-//     this.root.state = mergedState
-//   }
-// }
-
-// export const store = new Store({
-//   text: null
-// })
